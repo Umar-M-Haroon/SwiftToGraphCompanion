ba4572ab99053e291ea5d3a2844fff1653e7bb3a
Umar Haroon <umar.m.haroon@gmail.com>
2023-02-22 14:02:45 -0700

    Initial Commit

---
+++ SwiftToGraphMacTool/SwiftToGraphMacToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftToGraphMacToolApp.swift
+//  SwiftToGraphMacTool
+//
+//  Created by Umar Haroon on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftToGraphMacToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
